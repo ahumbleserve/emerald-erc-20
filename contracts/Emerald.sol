723002248aae8931cc64dc0e274dbf2685294e44
Vitor Jesus <ahumbleserve@yahoo.com.br>
2024-07-09 11:49:04 -0300

    added transfer method

--- contracts/Emerald.sol
+++ contracts/Emerald.sol
@@ -6,4 +6,36 @@ pragma solidity ^0.8.24;
 
 contract Emerald {
     
+    string public name = "Emerald";
+    string public symbol = "EMRLD";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 10000 * 10 ** 18;
+    mapping (address => uint256) private _balances;
+    
+    constructor(){
+        _balances[msg.sender] = totalSupply;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _expender, uint256 _value);
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+        return _balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        require(balanceOf(msg.sender) >= _value, "Insufficient funds");
+
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        
+    }
+
 }
--- test/Emerald.test.ts
+++ test/Emerald.test.ts
@@ -13,12 +13,49 @@ describe("Emerald Tests", function () {
     return { emerald, owner, otherAccount };
   }
 
-  it("Should test", async function () {
-      const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
-
-      expect(true).to.equal(true);
+  it("Should have correct name", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const name = await emerald.name();
+    expect(name).to.equal("Emerald");
   });
 
+  it("Should have correct symbol", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const symbol = await emerald.symbol();
+    expect(symbol).to.equal("EMRLD");
+  });
 
+  it("Should have correct decimals", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const decimals = await emerald.decimals();
+    expect(decimals).to.equal(18);
+  });
+
+  it("Should have correct totalSupply", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const totalSupply = await emerald.totalSupply();
+    expect(totalSupply).to.equal(10000n * 10n ** 18n);
+  });
+
+  it("Should have get balance", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const balance = await emerald.balanceOf(owner.address)
+    expect(balance).to.equal(10000n * 10n ** 18n);
+  });
+
+  it("Should transfer", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    await emerald.transfer(otherAccount, 1n);
+
+    const balance = await emerald.balanceOf(otherAccount)
+    expect(balance).to.equal(1n);
+  });
+
+  it("Should NOT transfer", async function () {
+    const { emerald, owner, otherAccount } = await loadFixture(deployFixture);
+    const instance = emerald.connect(otherAccount);
+
+    await expect(instance.transfer(owner, 1n)).to.be.revertedWith("Insufficient funds");
+  });
 
 });
